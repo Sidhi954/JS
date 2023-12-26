712b01294127bea9fb4ac7dbab6fff82fe29d7d2
Sidhi954 <79316354+Sidhi954@users.noreply.github.com>
2023-12-26 18:29:49 +0000

    Arrays

---
+++ 02_basics2/01_array.js
@@ -0,0 +1,11 @@
+const myArr=[9,6,5,7,2]
+console.log(myArr[3])
+
+const myArr2=[true,2,8,43,"Sidhi","Suman"]
+console.log(myArr2[0])
+console.log(myArr2[3])
+console.log(myArr2[4])
+
+myArr.push(3)
+myArr.push(4)
+console.log(myArr)
