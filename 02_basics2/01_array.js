cd67853764135fe0aeb7546edd434464f2e1830e
Sidhi954 <79316354+Sidhi954@users.noreply.github.com>
2023-12-28 17:42:02 +0000

    Arrays

--- 02_basics2/01_array.js
+++ 02_basics2/01_array.js
@@ -9,3 +9,24 @@ console.log(myArr2[4])
 myArr.push(3)
 myArr.push(4)
 console.log(myArr)
+myArr.pop()
+console.log(myArr)
+
+console.log(myArr.includes(2))
+console.log(myArr2.indexOf("Sidhi"))
+
+console.log(myArr.unshift(8))
+console.log(myArr)
+console.log(myArr.shift())
+console.log(myArr)
+
+//slice and splice in Array
+console.log("A", myArr)
+const ar1=myArr.slice(1,4)
+console.log("B", ar1)
+
+const ar2=myArr.splice(1,4)
+console.log("C",ar2)
+console.log("D", myArr)
+
+//splice: it changes the content of the array by changing/replacing/removing existing elements and/or adding the elements.
