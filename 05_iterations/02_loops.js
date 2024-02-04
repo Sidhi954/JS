654bebc646bcefefb98fd62a11d849df4a975892
Sidhi954 <79316354+Sidhi954@users.noreply.github.com>
2024-02-04 18:39:48 +0000

    While and do-while loop

---
+++ 05_iterations/02_loops.js
@@ -0,0 +1,19 @@
+//while loop
+let index=1
+while(index<10){
+    console.log(index);
+    index+=2
+}
+
+let arr=["jaipur","hyderabad","delhi","bangalore"]
+let i=0
+while(i<arr.length){
+    console.log(arr[i])
+    i++;
+}
+
+//do-while loop
+let score=11
+do{
+    console.log(`value of score is ${score}`)
+}while(score<=8)
