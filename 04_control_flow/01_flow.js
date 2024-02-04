b67637593325044633cedac430059c50016b8d08
Sidhi954 <79316354+Sidhi954@users.noreply.github.com>
2024-02-04 16:48:16 +0000

    Control flow

--- 04_control_flow/01_flow.js
+++ 04_control_flow/01_flow.js
@@ -6,3 +6,35 @@ if(true){
 if(8!=43){
     console.log("executed")
 }
+//"===" it checks equality with the datatype 
+if(2==="2"){
+    console.log("Executed with type")
+}
+else{
+    console.log("outside if")
+}
+
+const balance=6000
+
+if(balance <6000){
+    console.log(`balance less than 6000`)
+}
+else if(balanc=6000){
+    console.log(`balance is 6000`)
+}
+else{
+    console.log(`balance is greater`)
+}
+
+
+// &&-->and    ||--->or
+const userloggedIn=true
+const loggedInEmail=true
+const loggedInGoogle=false
+
+if(userloggedIn && loggedInEmail){
+    console.log(`User is logged in`)
+}
+if(userloggedIn || loggedInGoogle){
+    console.log("Log in by using gmail account")
+}
