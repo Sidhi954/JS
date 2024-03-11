4e4b47aaf693ef7f662e350864fcbae49a1d00d3
Sidhi Raj <siddhir954@gmail.com>
2024-03-11 22:15:11 +0530

    Objects

--- 02_basics2/03_objects.js
+++ 02_basics2/03_objects.js
@@ -1,4 +1,4 @@
-//object literals
+//object literals:-Create a single object, using an object literal.Using an object literal, you both define and create an object in one statement.
 const JSuser={
     naam:"Sidhi",
     age:16,
@@ -9,9 +9,18 @@ const JSuser={
 console.log(JSuser)
 console.log(JSuser["age"])
 
-
+//objects:-one of the javascript datatypes.
+// Booleans can be objects (if defined with the new keyword)
+// Numbers can be objects (if defined with the new keyword)
+// Strings can be objects (if defined with the new keyword)
+// Dates are always objects
+// Maths are always objects
+// Regular expressions are always objects
+// Arrays are always objects
+// Functions are always objects
+// Objects are always objects
 JSuser.location="bengaluru"
-Object.freeze(JSuser)
+//Object.freeze(JSuser)
 console.log(JSuser)
 
 //After freezing the object we cannot change its value
@@ -23,8 +32,10 @@ JSuser.greeting = function(){
 }
 
 JSuser.greetingTwo = function(){
-    console.log(`Hello our JS user, ${this.name}`);
+    console.log(`Hello our JS user, ${this.naam}`);
 }
 
 console.log(JSuser.greeting());
 console.log(JSuser.greetingTwo());
+
+//sometimes to access a variable we have to use square bracket instead of (.)
